03556dbf347952b8fe8a9e6c898bfbdcde2f82b4
Valon Rama <valon.rama@creditgenie.com>
2023-01-14 17:24:39 -0500

    Cleaned up aws sso login script. Made some tweaks to ci/cd flow

--- aws-sso-login.sh
+++ aws-sso-login.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
 
-AWS_PROFILE=vrama-root
+export AWS_PROFILE=vrama-root
 
 echo "Logging into AWS SSO account $AWS_PROFILE..."
 
-CALLER_IDENTITY=$(aws sts get-caller-identity --profile ${AWS_PROFILE})
-echo $CALLER_IDENTITY
+CALLER_IDENTITY=$(aws sts get-caller-identity)
+ERROR_STRING="Error loading SSO Token: The SSO access token has either expired or is otherwise invalid."
 
-if echo "$CALLER_IDENTITY" | grep -q "InvalidClientTokenId"; then
-    echo "Access token is invalid, exiting..."
-    exit 1
-elif echo "$CALLER_IDENTITY" | grep -q "ExpiredToken"; then
-    echo "Access token is expired, logging in via SSO..."
+if echo $CALLER_IDENTITY | grep -q "$ERROR_STRING"; then
+  echo "SSO token expired, logging in again..."
+  aws sso login --profile $AWS_PROFILE
 fi
-
-export AWS_PROFILE=$AWS_PROFILE
-
-aws sso login --profile $AWS_PROFILE
 
 ssocreds -p $AWS_PROFILE
