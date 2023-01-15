607ba42f0863bdd0c221ba918d8a634c278104f3
Valon Rama <valon.rama@creditgenie.com>
2023-01-15 14:04:41 -0500

    Cleanups

--- lib/sheriff-sale-scraper-stack.ts
+++ lib/sheriff-sale-scraper-stack.ts
@@ -2,14 +2,13 @@ import { config } from 'dotenv';
 config();
 
 import { Construct } from 'constructs';
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
+import { Duration, RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
 import * as events from 'aws-cdk-lib/aws-events';
 import * as eventTargets from 'aws-cdk-lib/aws-events-targets';
 import * as iam from 'aws-cdk-lib/aws-iam';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as lambdaEventSources from 'aws-cdk-lib/aws-lambda-event-sources';
 import * as lambdaNodeJs from 'aws-cdk-lib/aws-lambda-nodejs';
-// import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -25,37 +24,10 @@ export class SheriffSaleScraperStack extends Stack {
     if (!ENV) throw new Error('ENV not set');
     if (!DATABASE_URL) throw new Error('DATABASE_URL not set');
 
-    // const vpc = new ec2.Vpc(this, 'SheriffSaleVPC', {
-    //   cidr: '10.0.0.0/16',
-    //   maxAzs: 3,
-    //   natGateways: ENV === 'prod' ? 2 : 1,
-    //   subnetConfiguration: [
-    //     {
-    //       name: 'sheriff-sale-private-subnet',
-    //       subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
-    //       cidrMask: 24,
-    //     },
-    //     {
-    //       name: 'sheriff-sale-public-subnet',
-    //       subnetType: ec2.SubnetType.PUBLIC,
-    //       cidrMask: 24,
-    //     },
-    //   ],
-    // });
-
-    // const newJerseySheriffSaleSecurityGroup = new ec2.SecurityGroup(this, 'NewJerseySheriffSaleSecurityGroup', {
-    //   allowAllOutbound: true,
-    //   description: 'Security group for New Jersey Sheriff Sale Scraper',
-    //   securityGroupName: 'new-jersey-sheriff-sale-security-group',
-    //   vpc,
-    // });
-
-    // newJerseySheriffSaleSecurityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(443), 'Allow HTTPS traffic');
-    // newJerseySheriffSaleSecurityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80), 'Allow HTTP traffic');
-
     const newJerseySheriffSaleBucket = new s3.Bucket(this, 'NewJerseySheriffSaleBucket', {
       bucketName: `nj-sheriff-sale-bucket-${ENV}`,
       encryption: s3.BucketEncryption.S3_MANAGED,
+      removalPolicy: ENV === 'prod' ? RemovalPolicy.RETAIN : RemovalPolicy.DESTROY,
     });
 
     const newJerseySheriffSaleListingParserDLQ = new sqs.Queue(this, 'NewJerseySheriffSaleListingParserDLQ', {
@@ -126,12 +98,7 @@ export class SheriffSaleScraperStack extends Stack {
         roleName: `new-jersey-sheriff-sale-scraper-role-${ENV}`,
       }),
       runtime: lambda.Runtime.NODEJS_16_X,
-      // securityGroups: [newJerseySheriffSaleSecurityGroup],
       timeout: Duration.minutes(15),
-      // vpc,
-      // vpcSubnets: {
-      //   subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
-      // },
     });
 
     const newJerseySheriffSaleListingParser = new lambdaNodeJs.NodejsFunction(
